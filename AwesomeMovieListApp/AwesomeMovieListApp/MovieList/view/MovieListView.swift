b6a2441fc27db20368690587e6e5ec4301788396
Tomaz Rocha Silva <silva.tr0@gmail.com>
2018-10-27 15:22:24 -0300

    First commit, creating app folder structure

---
+++ AwesomeMovieListApp/AwesomeMovieListApp/MovieList/view/MovieListView.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AwesomeMovieListApp
+//
+//  Created by Tomaz Rocha Silva on 23/10/18.
+//  Copyright © 2018 Tomaz Rocha Silva. All rights reserved.
+//
+
+import UIKit
+
+class MovieListView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
